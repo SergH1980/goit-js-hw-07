f316ff2d17a58036348098ec0a635a906ad2b14a
SergH1980 <serg.galaka@gmail.com>
2023-02-19 16:21:06 +0200

    task1 completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(`.gallery`);
+let modal;
+const imageEl = galleryItems
+  .map((image) => {
+    return `<div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+          <img
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+          />
+        </a>
+      </div>`;
+  })
+  .join(``);
+galleryContainer.insertAdjacentHTML(`beforeend`, imageEl);
+
+galleryContainer.addEventListener(`click`, onImageClick);
+function onImageClick(event) {
+  if (event.target.nodeName !== `IMG`) {
+    return;
+  }
+  event.preventDefault();
+  createModal(event);
+}
+function createModal(data) {
+  const modalImgLink = data.target.dataset.source;
+  const modalImgAlt = data.target.getAttribute(`alt`);
+
+  modal = basicLightbox.create(
+    `
+    <img src="${modalImgLink}" alt="${modalImgAlt}">
+`
+  );
+  modal.show();
+}
+
+galleryContainer.addEventListener(`keydown`, closeModal);
+function closeModal(data) {
+  if (data.code === "Escape") {
+    modal.close();
+  }
+}
